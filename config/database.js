3cfb65d1db266375076fe8dd57938abe7ade201b
Alessandro <alessandroadm@live.com>
2020-08-25 14:59:57 -0300

    feat(database): conection

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const path = require('path');
+require('dotenv').config({ path:path.join(__dirname, '.env') });
+const mysql = require('mysql2');
+
+const conect = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  waitForConnections: true,
+  connectionLimit: 10,
+  multipleStatements: true,
+  queueLimit: 0
+});
+module.exports = conect;
